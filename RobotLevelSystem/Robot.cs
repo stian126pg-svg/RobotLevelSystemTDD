27a912cb2a48c311a5aebc7e55522889bfd41bda
stian126jg-svg <stian1.26jg@gmail.com>
2026-09-03 13:34:31 +0200

    feat: increase robot level after gaining experience

--- RobotLevelSystem/Robot.cs
+++ RobotLevelSystem/Robot.cs
@@ -2,12 +2,13 @@
 
 public class Robot
 {
-    public int Level { get; } = 1;
+    public int Level { get; private set; } = 1;
 
     public int ExperiencePoints { get; private set; }
 
     public void AddExperience(int amount)
     {
         ExperiencePoints += amount;
+        Level++;
     }
 }
